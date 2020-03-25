c8d8ec0ac5bd562bacbcedd446a1406de48cea64
Yoshinori Kosaka <yoshinori.kosaka@welyco.com>
2020-03-26 01:03:03 +0900

    fix: move files

--- scraip.rb
+++ scraip.rb
@@ -5,7 +5,7 @@ lines = file.readlines
 lines.each do |line|
   md_path = line.chomp + ".md"
   open md_path do |md|
-    w = File.open(md_path.split("/").last, "w")
+    w = File.open("md/" + md_path.split("/").last, "w")
     w.write md.read
     w.close
   end
